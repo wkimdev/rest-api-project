77c2c0023803cca7aee74d3cbc34296566d755d7
wkimdev <32521173+wkimdev@users.noreply.github.com>
2018-08-14 00:47:54 +0900

    Task : first update

---
+++ rest-cnt-project/src/main/java/com/doublechain/ico/RestCntProjectApplication.java
@@ -0,0 +1,12 @@
+package com.doublechain.ico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCntProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCntProjectApplication.class, args);
+	}
+}
---
+++ rest-cnt-project/src/main/java/com/doublechain/ico/service/WalletVO.java
@@ -0,0 +1,14 @@
+package com.doublechain.ico.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WalletVO {
+	
+	private int Id;
+	private String address;
+	private String key;
+
+}
