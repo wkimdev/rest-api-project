760f7a80b5353d11f9d0ae043c8e19ac00e93d8f
wkimdev <32521173+wkimdev@users.noreply.github.com>
2018-08-14 22:50:21 +0900

    Task : conflict update

---
+++ src/main/java/com/doublechain/ico/controller/TestController.java
@@ -0,0 +1,43 @@
+package com.doublechain.ico.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.doublechain.ico.service.WalletVO;
+
+@RestController
+@RequestMapping(value = "api")
+public class TestController {
+	
+	@GetMapping
+	public WalletVO get(){
+		WalletVO walletVO = new WalletVO();
+		walletVO.setId(1);
+		walletVO.setAddress("AKJDFLKJDSALFJ");
+		walletVO.setKey("oxoddksajflkdj");
+		return walletVO;
+	}
+	
+	@GetMapping("args")
+	public WalletVO getArgs(@RequestParam(value = "Id")int Id, 
+							@RequestParam(value = "address")String address){
+		
+		WalletVO walletVO = new WalletVO();
+		
+		return null;
+	}
+	
+	//post
+	// requestbody 는 언제 쓰는지, 어떻게 쓰는지.
+	@PostMapping
+	public WalletVO post(@RequestBody WalletVO param){
+		return param;
+	}
+	
+	
+	
+}
